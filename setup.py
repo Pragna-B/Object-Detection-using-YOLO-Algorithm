c0354ad23b46018dda9e7f31c76a6ef0d0eaa0cd
Pragna B <pragnabheemaraju02@gmail.com>
2024-04-16 16:49:53 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+setup(
+    name= 'Industry_safety_Dectection',
+    version = '0.0.0',
+    author = 'Pragna',
+    author_email ='pragnabheemaraju02@gmail.com',
+    packages= find_packages(),
+    install_requires=[]
+    
+    
+)
